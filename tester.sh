5e2bc0a8a9ad15b1db9f2fc9f5e01413b85c25c7
Jabez Wilson <jabez.wilson0@gmail.com>
2017-05-30 13:48:03 +1000

    added titles to termite windows

--- tester.sh
+++ tester.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-termite --exec "/home/jabez/Uni/3331/asst2/Dvr A 2000 Top1/configA.txt" &
-termite --exec "/home/jabez/Uni/3331/asst2/Dvr B 2001 Top1/configB.txt" &
-termite --exec "/home/jabez/Uni/3331/asst2/Dvr C 2002 Top1/configC.txt" &
-termite --exec "/home/jabez/Uni/3331/asst2/Dvr D 2003 Top1/configD.txt" &
-termite --exec "/home/jabez/Uni/3331/asst2/Dvr E 2004 Top1/configE.txt" &
-termite --exec "/home/jabez/Uni/3331/asst2/Dvr F 2005 Top1/configF.txt" &
+termite --exec "/home/jabez/Uni/3331/asst2/Dvr A 2000 Top1/configA.txt" --title="Node A" &
+termite --exec "/home/jabez/Uni/3331/asst2/Dvr B 2001 Top1/configB.txt" --title="Node B" &
+termite --exec "/home/jabez/Uni/3331/asst2/Dvr C 2002 Top1/configC.txt" --title="Node C" &
+termite --exec "/home/jabez/Uni/3331/asst2/Dvr D 2003 Top1/configD.txt" --title="Node D" &
+termite --exec "/home/jabez/Uni/3331/asst2/Dvr E 2004 Top1/configE.txt" --title="Node E" &
+termite --exec "/home/jabez/Uni/3331/asst2/Dvr F 2005 Top1/configF.txt" --title="Node F" &
